5301feed4e882bddb2a1b4af60f49395c5c09708
oso1248 <oso1248@gmail.com>
2021-08-02 23:21:01 -0600

    fixing course seed data

--- pages/site/serviceworker.js
+++ pages/site/serviceworker.js
@@ -4,7 +4,7 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-var CACHE_STATIC = 'static-v016';
+var CACHE_STATIC = 'static-v017';
 
 self.addEventListener('install', function (event) {
   self.skipWaiting();
